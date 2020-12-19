b1173e3c1e2ff8d5dd5a7abb8335b9c97ff38a47
Willian Velhos <williancorreavelhos@gmail.com>
2020-12-19 08:45:27 -0300

    teste

---
+++ XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1 : IAsyncLifetime
+    {
+        public const string DATABASE_NAME_PLACEHOLDER = "@@databaseName@@";
+        private string _dockerContainerId;
+        private string _dockerSqlPort;
+
+        public UnitTest1()
+        {
+        }
+
+        public async Task InitializeAsync()
+        {
+            (_dockerContainerId, _dockerSqlPort) = await DockerSqlDatabaseUtilities.EnsureDockerStartedAndGetContainerIdAndPortAsync();
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
