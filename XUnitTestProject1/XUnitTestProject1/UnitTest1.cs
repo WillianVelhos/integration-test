66970fa8c506769ebbb71faea35c9beaba16ef75
Willian Velhos <williancorreavelhos@gmail.com>
2020-12-19 19:27:52 -0300

    teste

--- XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Management.Automation.Runspaces;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,9 +18,22 @@ namespace XUnitTestProject1
 
         public async Task InitializeAsync()
         {
-            (_dockerContainerId, _dockerSqlPort) = await DockerSqlDatabaseUtilities.EnsureDockerStartedAndGetContainerIdAndPortAsync();
-        }
+            // (_dockerContainerId, _dockerSqlPort) = await DockerSqlDatabaseUtilities.EnsureDockerStartedAndGetContainerIdAndPortAsync();
 
+            var runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            var pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript("");
+            pipeline.Commands.Add("Out-String");
+            var results = pipeline.Invoke();
+            runspace.Close();
+
+            var stringBuilder = new StringBuilder();
+            foreach (var result in results)
+                stringBuilder.AppendLine(result.ToString());
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
 
         [Fact]
         public void Test1()
