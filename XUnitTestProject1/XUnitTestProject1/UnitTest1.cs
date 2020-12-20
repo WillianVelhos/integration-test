a7323d0e7ae9c9d9e65e8ec1d6512ca7d751bed3
Willian Correa Velhos <willian.velhos@cwi.com.br>
2020-12-19 22:05:00 -0300

    teste 1000

--- XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Management.Automation.Runspaces;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,8 @@ namespace XUnitTestProject1
         public const string DATABASE_NAME_PLACEHOLDER = "@@databaseName@@";
         private string _dockerContainerId;
         private string _dockerSqlPort;
+        public const string SQLSERVER_SA_PASSWORD = "yourStrong(!)Password";
+
 
         public UnitTest1()
         {
@@ -20,25 +24,60 @@ namespace XUnitTestProject1
         {
             // (_dockerContainerId, _dockerSqlPort) = await DockerSqlDatabaseUtilities.EnsureDockerStartedAndGetContainerIdAndPortAsync();
 
+
+            var containerName = "IntegrationTests" + Guid.NewGuid();
+
             var runspace = RunspaceFactory.CreateRunspace();
             runspace.Open();
             var pipeline = runspace.CreatePipeline();
-            pipeline.Commands.AddScript("");
+            pipeline.Commands.AddScript($"docker run --name {containerName}  -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD={SQLSERVER_SA_PASSWORD}' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest");
             pipeline.Commands.Add("Out-String");
             var results = pipeline.Invoke();
             runspace.Close();
 
-            var stringBuilder = new StringBuilder();
-            foreach (var result in results)
-                stringBuilder.AppendLine(result.ToString());
+            var dale = results.First();
+             _dockerContainerId = results.First().ToString().Replace("\r\n","");
 
-            Console.WriteLine(stringBuilder.ToString());
+            Console.WriteLine("ContainerId: " _dockerContainerId);
+
+            await WaitUntilDatabaseAvailableAsync("1433");
         }
 
         [Fact]
         public void Test1()
         {
 
+        }
+
+        private static async Task WaitUntilDatabaseAvailableAsync(string databasePort)
+        {
+            var start = DateTime.UtcNow;
+            const int maxWaitTimeSeconds = 300;
+            var connectionEstablised = false;
+            while (!connectionEstablised && start.AddSeconds(maxWaitTimeSeconds) > DateTime.UtcNow)
+            {
+                try
+                {
+                    var sqlConnectionString = $"Data Source=localhost,{databasePort};Integrated Security=False;User ID=SA;Password={SQLSERVER_SA_PASSWORD}";
+                    using var sqlConnection = new SqlConnection(sqlConnectionString);
+                    await sqlConnection.OpenAsync();
+                    connectionEstablised = true;
+
+                    Console.WriteLine("Conexão aberta!");
+                }
+                catch
+                {
+                    // If opening the SQL connection fails, SQL Server is not ready yet
+                    await Task.Delay(500);
+                }
+            }
+
+            if (!connectionEstablised)
+            {
+                throw new Exception("Connection to the SQL docker database could not be established within 60 seconds.");
+            }
+
+            return;
         }
 
         public Task DisposeAsync()
